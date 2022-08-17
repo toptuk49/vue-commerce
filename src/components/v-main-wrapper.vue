2b87271923eab4063d2c3248cbf0a859925deff5
toptuk49 <chyka228tigra@yandex.ru>
2022-08-17 09:07:05 +0300

    Successfully initialized app with all packages

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="v-main-wrapper">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-main-wrapper'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
